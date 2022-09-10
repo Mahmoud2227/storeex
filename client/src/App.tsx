13bebf7932d9568d220171b003be8fe85e448be4
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-10 20:42:39 +0200

    initial react setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className=''>
+				Hello Word
+				<Routes></Routes>
+			</div>
+		</BrowserRouter>
+	);
+};
+
+export default App;
