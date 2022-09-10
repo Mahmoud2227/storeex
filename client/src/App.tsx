350e500e20e8b0c056241db4e14e61b25f7a4eb2
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-10 21:33:06 +0200

    Add Footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Header />
-			<main className="container mx-auto">
+			<main className='container mx-auto min-h-[80vh]'>
 				<h1>Welcome to Storeex</h1>
 			</main>
 			<Routes></Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 };
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className='container mx-auto'>
+				<p className="w-fit mx-auto">Copyright &copy; Storeex</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
