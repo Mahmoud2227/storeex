f65988646ebe53bd35cb49c0508be4dcdecd79c9
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-13 01:55:20 +0200

    initialize the mongodb database

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 import products from "./DummyProducts";
 
 dotenv.config();
+connectDB();
+
 const app = express();
 
 app.use(cors());
