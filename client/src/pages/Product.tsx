d62c13d03cec694f2b214263a4abb166133b3a01
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-12 18:39:39 +0200

    add the checkout section to the product page

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -7,22 +7,42 @@ const Product: React.FC = () => {
 	const {id} = useParams<{id: string}>();
 	const product = products.find((p) => p._id.toString() === id);
 	return (
-		<div>
+		<>
 			<Link to='/' className='block w-fit mb-4 p-4 font-medium hover:bg-slate-300'>
 				GO BACK
 			</Link>
-			<div className="flex gap-16">
-				<img src={product?.image} alt={product?.name} />
-				<div>
-					<h1 className="text-3xl font-bold py-8">{product?.name}</h1>
-					<div>
-						{product && <Rating value={product?.rating} text={`${product?.numReviews} reviews`} className='border-t-2 py-3' />}
-            <p className="font-medium py-3 border-t-2">Price: {product?.price}</p>
-            <p className="py-3 border-t-2">Description: {product?.description}</p>
+			{product && (
+				<div className='flex justify-center gap-8 flex-wrap'>
+					<div className="min-w-[250px] flex-1">
+						<img src={product?.image} alt={product?.name} />
+					</div>
+					<div className='flex-1 min-w-[300px]'>
+						<h1 className='text-3xl font-bold py-8'>{product?.name}</h1>
+						<div>
+							<Rating
+								value={product?.rating}
+								text={`${product?.numReviews} reviews`}
+								className='border-t-2 py-3'
+							/>
+							<p className='font-medium py-3 border-t-2'>Price: {product?.price}</p>
+							<p className='py-3 border-t-2'>Description: {product?.description}</p>
+						</div>
+					</div>
+					<div className='flex-1 min-w-[250px] max-w-sm h-fit border-2'>
+						<p className='p-3 border-b-2'>Price: {product?.price}</p>
+						<p className='p-3 border-b-2'>
+							Status: {product?.countInStock > 0 ? "In Stock" : "Out of Stock"}
+						</p>
+						<button
+							type='button'
+							className='w-[calc(100%-24px)] p-3 m-3 text-white bg-slate-700 disabled:bg-opacity-70 disabled:cursor-not-allowed'
+							disabled={product.countInStock === 0}>
+							Add to Cart
+						</button>
 					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 
