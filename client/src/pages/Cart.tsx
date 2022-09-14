8c7e606d7865de60ab276207922973a456264de6
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-15 01:11:47 +0200

    add checkout card

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -20,47 +20,68 @@ const Cart = () => {
 	const removeFromCartHandler = (id: string) => {
 		console.log(id);
 	};
+
+	const onCheckoutHandler = () => {
+		console.log("checkout");
+	};
 	return (
 		<>
-			<h1 className='text-3xl font-semibold mb-4'>Shopping Cart</h1>
+			<h1 className='text-3xl font-semibold mb-4'>SHOPPING CART</h1>
 			{cartItems.length === 0 ? (
 				<Message color='blue'>Your Cart Is Empty!</Message>
 			) : (
-				<div className='max-w-4xl'>
-					{cartItems.map((item) => (
-						<div key={item.product} className='grid grid-cols-12 p-4 border-b-2'>
-							<div className='col-span-8 flex'>
-								<div className='max-w-[80px]'>
-									<img src={item.image} alt={item.name} className='rounded-xl' />
+				<div className='flex flex-col lg:flex-row justify-between items-center lg:items-start gap-4'>
+					<div className='flex-1 max-w-5xl'>
+						{cartItems.map((item) => (
+							<div key={item.product} className='grid grid-cols-12 p-4 border-b-2'>
+								<div className='col-span-8 flex'>
+									<div className='max-w-[80px]'>
+										<img src={item.image} alt={item.name} className='rounded-xl' />
+									</div>
+									<h2 className='ml-4 font-medium'>
+										<Link to={`/product/${item.product}`}>{item.name}</Link>
+									</h2>
 								</div>
-								<h2 className='ml-4 font-medium'>
-									<Link to={`/product/${item.product}`}>{item.name}</Link>
-								</h2>
+								<p className='col-span-2 justify-between ml-4 font-medium'>
+									{item.qty} x ${item.price}
+								</p>
+								<div className='col-span-2 flex justify-between '>
+									<select
+										className='w-fit h-fit px-6 py-2 bg-slate-100'
+										name='qty'
+										title='Qty select'
+										value={item.qty}
+										onChange={(e) => dispatch(addProductToCart(item.product, +e.target.value))}>
+										{Array.from(Array(item.countInStock).keys()).map((x) => (
+											<option key={x + 1} value={x + 1}>
+												{x + 1}
+											</option>
+										))}
+									</select>
+									<span
+										className='p-2 h-fit hover:bg-red-50 cursor-pointer'
+										onClick={removeFromCartHandler.bind(null, item.product)}>
+										<AiFillDelete className='text-2xl text-red-500' />
+									</span>
+								</div>
+								<div></div>
 							</div>
-							<p className='col-span-2 justify-between ml-4 font-medium'>
-								{item.qty} x ${item.price}
-							</p>
-							<div className='col-span-2 flex justify-between '>
-								<select
-									className='w-fit h-fit px-6 py-2 bg-slate-100'
-									name='qty'
-									title='Qty select'
-									value={item.qty}
-									onChange={(e) => dispatch(addProductToCart(item.product, +e.target.value))}>
-									{Array.from(Array(item.countInStock).keys()).map((x) => (
-										<option key={x + 1} value={x + 1}>
-											{x + 1}
-										</option>
-									))}
-								</select>
-								<span
-									className='p-2 h-fit hover:bg-red-50 cursor-pointer'
-									onClick={removeFromCartHandler.bind(null, item.product)}>
-									<AiFillDelete className='text-2xl text-red-500' />
-								</span>
-							</div>
-						</div>
-					))}
+						))}
+					</div>
+					<div className='w-full max-w-lg lg:max-w-xs h-fit p-4 border-2'>
+						<h2 className='text-2xl tracking-wide font-medium'>
+							SUBTOTAL ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) ITEMS
+						</h2>
+						<p className='font-medium my-4'>
+							${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+						</p>
+						<button
+							type='button'
+							className='w-full py-3 bg-slate-700 text-white font-medium tracking-wide'
+							onClick={onCheckoutHandler}>
+							Proceed To Checkout
+						</button>
+					</div>
 				</div>
 			)}
 		</>
