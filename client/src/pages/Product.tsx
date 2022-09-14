7cf1191029c991df19501406d12505c725e90326
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-14 22:28:47 +0200

    add option fo selecting product qty

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,19 +1,25 @@
-import React from "react";
-import {Link, useParams} from "react-router-dom";
+import {useState, useEffect, FC} from "react";
+import {Link, useParams,useNavigate} from "react-router-dom";
 import Rating from "../components/UI/Rating";
 import Spinner from "../components/UI/Spinner";
 import Message from "../components/UI/Message";
 import {useAppDispatch, useAppSelector} from "../hooks/RTK";
 import {fetchProductDetails} from "../store/slices/productDetails";
 
-const Product: React.FC = () => {
+const Product: FC = () => {
+	const [qty, setQty] = useState(1);
 	const {id} = useParams<{id: string}>();
 	const {error, loading, productDetails} = useAppSelector((state) => state.productDetails);
 	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 
-	React.useEffect(() => {
+	useEffect(() => {
 		dispatch(fetchProductDetails(id!));
 	}, [dispatch, id]);
+
+	const addToCartHandler = () => {
+		navigate(`/cart/${id}?qty=${qty}`);
+	}
 
 	return (
 		<>
@@ -42,14 +48,34 @@ const Product: React.FC = () => {
 						</div>
 					</div>
 					<div className='flex-1 min-w-[250px] max-w-sm h-fit border-2'>
-						<p className='p-3 border-b-2'>Price: {productDetails?.price}</p>
-						<p className='p-3 border-b-2'>
-							Status: {productDetails?.countInStock! > 0 ? "In Stock" : "Out of Stock"}
+						<p className='flex justify-between py-3 px-5 border-b-2'>
+							Price: <span>${productDetails?.price}</span>
 						</p>
+						<p className='flex justify-between py-3 px-5 border-b-2'>
+							Status:
+							{productDetails?.countInStock! > 0 ? (
+								<span>In Stock</span>
+							) : (
+								<span>Out of Stock</span>
+							)}
+						</p>
+						{productDetails?.countInStock! > 0 && (
+							<div className='flex justify-between py-3 px-5 border-b-2'>
+								<label htmlFor='qty'>Qty</label>
+								<select className='bg-slate-100 p-2' onChange={(e)=>setQty(+e.target.value)}>
+									{Array.from(Array(productDetails?.countInStock!).keys()).map((x) => (
+										<option key={x} value={x+1}>
+											{x + 1}
+										</option>
+									))}
+								</select>
+							</div>
+						)}
 						<button
 							type='button'
 							className='w-[calc(100%-24px)] p-3 m-3 text-white bg-slate-700 disabled:bg-opacity-70 disabled:cursor-not-allowed'
-							disabled={productDetails?.countInStock === 0}>
+							disabled={productDetails?.countInStock === 0}
+							onClick={addToCartHandler}>
 							Add to Cart
 						</button>
 					</div>
