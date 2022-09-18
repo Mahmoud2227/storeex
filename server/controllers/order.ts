0f77ea5e7c6620779796581ed3dd0bf5cd95b2da
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-18 21:53:30 +0200

    implement get order by id route

--- server/controllers/order.ts
+++ server/controllers/order.ts
@@ -1,4 +1,4 @@
-import { Request } from "express";
+import {Request} from "express";
 import asyncHandler from "express-async-handler";
 import Order from "../models/Order";
 import IUser from "../types/User";
@@ -38,5 +38,16 @@ export const addOrderItems = asyncHandler(async (req: RequestWithUser, res) => {
 
 		const createdOrder = await order.save();
 		res.status(201).json(createdOrder);
+	}
+});
+
+export const getOrderById = asyncHandler(async (req: RequestWithUser, res) => {
+	const order = await Order.findById(req.params.id).populate("user", "name email");
+
+	if (order) {
+		res.json(order);
+	} else {
+		res.status(404);
+		throw new Error("Order not found");
 	}
 });
--- server/routes/order.ts
+++ server/routes/order.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { addOrderItems } from "../controllers/order";
+import {addOrderItems, getOrderById} from "../controllers/order";
 import protect from "../middleware/Auth";
 
 const router = express.Router();
 
 router.post("/", protect, addOrderItems);
+router.get("/:id", protect, getOrderById);
 
 export default router;
