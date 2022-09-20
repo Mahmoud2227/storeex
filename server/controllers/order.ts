179a838f32df8c0fc7c4494a5b10f284152524ca
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-20 19:17:36 +0200

    implement get logged in user orders route

--- server/controllers/order.ts
+++ server/controllers/order.ts
@@ -52,6 +52,10 @@ export const getOrderById = asyncHandler(async (req: RequestWithUser, res) => {
 	}
 });
 
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+
 export const updateOrderToPaid = asyncHandler(async (req: RequestWithUser, res) => {
 	const order = await Order.findById(req.params.id);
 
@@ -71,4 +75,13 @@ export const updateOrderToPaid = asyncHandler(async (req: RequestWithUser, res) 
 		res.status(404);
 		throw new Error("Order not found");
 	}
+});
+
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+
+export const getMyOrders = asyncHandler(async (req: RequestWithUser, res) => {
+	const orders = await Order.find({user: req.user?._id});
+	res.json(orders);
 });
--- server/routes/order.ts
+++ server/routes/order.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {addOrderItems, getOrderById,updateOrderToPaid} from "../controllers/order";
+import {addOrderItems, getOrderById,updateOrderToPaid,getMyOrders} from "../controllers/order";
 import protect from "../middleware/Auth";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/", protect, addOrderItems);
 router.get("/:id", protect, getOrderById);
 router.put("/:id/pay", protect, updateOrderToPaid);
+router.get("/myorders", protect, getMyOrders);
 
 export default router;
